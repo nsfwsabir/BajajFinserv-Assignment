289c76e07a0235631b546228728cb799c3d1da1f
droidmystic <sabirkhan786ddn@gmail.com>
2026-04-24 18:14:22 +0530

    initial project setup

---
+++ src/main/java/com/bajajfinserv/sabir/assignment/leaderboard.java
@@ -0,0 +1,13 @@
+package com.bajajfinserv.sabir.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class leaderboard {
+
+	public static void main(String[] args) {
+		SpringApplication.run(leaderboard.class, args);
+	}
+
+}
---
+++ src/test/java/com/bajajfinserv/sabir/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajajfinserv.sabir.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
